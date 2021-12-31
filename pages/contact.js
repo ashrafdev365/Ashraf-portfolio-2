c04e3e5087c3503476ea53489164190017db3ac5
Ashraf <ashrafdev365@gmail.com>
2021-12-31 10:51:31 -0800

    add realtime database in project to send user request

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,45 @@
 import { DesktopNavigation } from "../Components/Navigation/DesktopNavigation";
-
+import { useState } from "react";
 const contact = () => {
+  const [form, setform] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  let name, value;
+  const handleForm = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setform({ ...form, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { name, email, subject, message } = form;
+    if (name && email && subject && message) {
+      const getData = await fetch(
+        "https://creative-portfolio-57669-default-rtdb.firebaseio.com/contact.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name, email, subject, message }),
+        }
+      );
+      console.log("data send");
+      setform({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+      });
+    } else {
+      alert("pleace fillup all the fileds");
+    }
+  };
   return (
     <>
       <h1 className="heading">
@@ -12,13 +51,12 @@ const contact = () => {
           <p>
             Lorem ipsum dolor sit amet consectetur amet, adipisicing elit. Quis,
             cupiditate dolor. Provident, sit! Sequi in recusandae, inventore
-            molestias, voluptates{" "}
+            molestias, voluptates
           </p>
 
           <div>
             <i className="fas fa-envelope-open"></i>
             <span>
-              {" "}
               <h3>Email</h3> <p>ashrafdev365@gmail.com</p>
             </span>
           </div>
@@ -39,20 +77,39 @@ const contact = () => {
           </span>
         </section>
 
-        <form action="">
+        <form action="POST" onSubmit={handleSubmit}>
           <span>
-            <input type="name" name="" placeholder="Name" />
-            <input type="email" name="" placeholder="Email" />
+            <input
+              type="name"
+              name="name"
+              onChange={handleForm}
+              value={form.name}
+              placeholder="Name"
+            />
+            <input
+              type="email"
+              name="email"
+              onChange={handleForm}
+              value={form.enail}
+              placeholder="Email"
+            />
           </span>
           <input
             type="text"
             className="subject"
-            name=""
+            name="subject"
+            onChange={handleForm}
+            value={form.subject}
             placeholder="Subject"
           />
-          <textarea name="" placeholder="Message"></textarea>
+          <textarea
+            name="message"
+            onChange={handleForm}
+            value={form.message}
+            placeholder="Message"
+          ></textarea>
 
-          <button>
+          <button type="submit">
             <span>Send</span> <i className="fas fa-paper-plane"></i>
           </button>
         </form>
